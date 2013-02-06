5dd69e624079c0619840a712a9e3f4c003b39c67
beetny <eq2aa@beetny.com>
2013-02-06 22:59:06 +1000

    Added property sheet. All build outputs go into build/bin

--- RemoteScreen/Tests/Tests/Test.cpp
+++ RemoteScreen/Tests/Tests/Test.cpp
@@ -3,7 +3,6 @@
 
 BOOST_AUTO_TEST_CASE( test1 )
 {
-    // reports 'error in "test1": test 2 == 1 failed'
     BOOST_CHECK( 2 == 1 );
 }
 
@@ -11,8 +10,6 @@ BOOST_AUTO_TEST_CASE( test2 )
 {
     int i = 0;
 
-    // reports 'error in "test2": check i == 2 failed [0 != 2]'
     BOOST_CHECK_EQUAL( i, 2 );
-
     BOOST_CHECK_EQUAL( i, 0 );
 }
